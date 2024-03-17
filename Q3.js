7a7ec3a24d8cb79da1810f12b0cc9f0cdf102fe7
Hamza <160007687+Hamza45677@users.noreply.github.com>
2024-03-18 01:39:57 +0500

    Assignment 3
    Assignment 3

---
+++ Q3.js
@@ -0,0 +1,6 @@
+// *3.* Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+export {};
+// let firstName: string = "hamza sohail"
+// console.log(firstName.toLowerCase())
+// console.log(firstName.toLocaleUpperCase())
+// console.log(firstName.charAt(0).toUpperCase() + firstName.slice(1));
